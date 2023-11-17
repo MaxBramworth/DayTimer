db7efdbe7c1074720f84b3ea996e4783602091ea
MaxBramworth <max.bramworth@gmail.com>
2023-11-17 12:28:16 +0000

    Added a csv parser and gave it validation to be able to check a file exits. Still working on the tests though

---
+++ DayTimerRedo.Tests/RepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using DayTimerRedo.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTimerRedo.Tests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void GivenAnInvalidFilePathway_WhenPathwaySet_DoesNotSet()
+        {
+            CSVParser parser = new();
+
+            parser.Pathway = "breaks";
+
+            Assert.AreNotEqual("breaks", parser.Pathway);
+        }
+
+        [TestMethod]
+        public void GivenAValidFilePathway_WhenPathwaySet_SetsThePathway()
+        {
+            CSVParser parser = new();
+
+            string expectedPathway = "../ITimeEventParser.cs";
+            parser.Pathway = expectedPathway;
+
+            Assert.IsTrue(File.Exists(expectedPathway));
+            Assert.AreEqual(expectedPathway, parser.Pathway);
+        }
+
+        [TestMethod]
+        public void GivenAValidFilePathwayWithInvalidType_WhenPathwaySet_DoesNotSet()
+        {
+            CSVParser parser = new();
+
+            parser.Pathway = "../DataBase/TimeEvents.json";
+
+            Assert.AreNotEqual("../DataBase/TimeEvents.json", parser.Pathway);
+        }
+    }
+}
---
+++ DayTimerRedo/Repository/CSVParser.cs
@@ -0,0 +1,34 @@
+﻿using DayTimerRedo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTimerRedo.Repository
+{
+    public class CSVParser : ITimeEventParser
+    {
+        private string _pathway = "DB not set";
+        public string Pathway { 
+            get => _pathway; 
+            set 
+            {
+                if (File.Exists(value)) //  && value.Substring(Math.Max(value.Length-3, 0)) == "csv"
+                {
+                    _pathway = value; 
+                }
+            } 
+        }
+        public ITimeEvent[] ReadAllTimeEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITimeEvent[] ReadAllTimeEvents(Func<ITimeEvent, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
