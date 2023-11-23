cb5e30554c77f0aab21722ec71ffcebafa145d03
MaxBramworth <max.bramworth@gmail.com>
2023-11-23 08:17:57 +0000

    Added notification service

---
+++ DayTimerRedo/Models/NotificationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace DayTimerRedo.Models
+{
+    public class NotificationService
+    {
+        private static void ShowNotification(string message)
+        {
+            new ToastContentBuilder().AddText(message).Show();
+        }
+    }
+}
